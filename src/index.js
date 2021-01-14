bae961f5219b65a067ac4c60afb3d073eb7aa6e0
Prashant sagar <prashant.sagar73@gmail.com>
2021-01-14 15:06:12 +0530

    mini projec -1 in the form of components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Aee from "./Aee" ;
-// import "./index.css";
+import Mini from "./Mini";
+import "./index.css";
 
-
-ReactDOM.render(<>
-<Aee/>
-<p>kha se aya mai hu kon?</p>
-
-<h3>mai hu mai hu </h3>
-
-</>,
-    document.getElementById('root'));
+ReactDOM.render(<Mini/>, document.getElementById("root"));
 
 
