eb79fb62fadfaee171f6e9a3880e69c897a9bf28
Prashant sagar <prashant.sagar73@gmail.com>
2021-01-16 00:05:44 +0530

    calc using  jsx

---
+++ src/Calc.jsx
@@ -0,0 +1,15 @@
+function add(a,b){
+    return a+b;
+}
+function sub(a,b) {
+    return a-b;
+}
+function Multiplication(a,b) {
+    let Multiplication = a*b;
+    return Multiplication;    
+}
+function Division(a,b){
+    return a%b;
+}
+
+export {add,sub,Multiplication,Division};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import name,{work,Passion} from "./List"
+import { add,sub,Multiplication,Division } from "./Calc"
+// import name,{work,Passion} from "./List"
 // import "./index.css";
 
 ReactDOM.render(
     <>
-    <ol>
-        <li>what is your name?</li>
-        <li>{name}</li>
-        <li>{work}</li>
-        <li>{Passion()}</li>
+    <ol>calc
+       <li>Sum of two number is: {add (3, 2)} </li>
+       <li>subtract of two number is: {sub (3, 2)} </li>
+       <li>Multiplication of two number is: {Multiplication (3, 2)} </li>
+       <li>Division of two number is: {Division (10, 2)} </li>
 
     </ol>
     </>, document.getElementById("root"));
