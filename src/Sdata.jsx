36910537ab0006271cf47d292ccd97f01ed1c471
Prashant sagar <prashant.sagar73@gmail.com>
2021-01-17 21:33:20 +0530

    basic layout with working condintion| 5 card

---
+++ src/Sdata.jsx
@@ -0,0 +1,33 @@
+const Sdata= [
+    {
+        sname: "DARK",
+        imgsrc:"https://source.unsplash.com/300x200/?netflix,dark",
+        title:" A Netflix original series",
+        link: "https://www.netflix.com/in/",
+    },
+    {
+        sname: "DARK",
+        imgsrc:"https://source.unsplash.com/300x200/?Netflix",
+        title:" A Netflix original series",
+        link:"https://www.netflix.com/in/",
+    },
+    {
+        sname: "DARK",
+        imgsrc:"https://source.unsplash.com/300x200/?Netflix,Dark",
+        title:" A Netflix original series",
+        link: "https://www.netflix.com/in/",
+    },
+    {
+        sname: "DARK",
+        imgsrc:"https://source.unsplash.com/300x200/?dark",
+        title:" A Netflix original series",
+        link: "https://www.netflix.com/in/",
+    },
+    {
+        sname: "DARK",
+        imgsrc:"https://source.unsplash.com/300x200/?series,dark",
+        title:" A Netflix original series",
+        link: "https://www.netflix.com/in/",
+    },
+];
+export default Sdata;
---
+++ src/card.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Card(props) {
+    // console.log(props);
+    return(
+        <>
+         <div className="cards">
+        <div className= "card">
+        <img src={props.imgsrc} alt="myPic" className="cardimg" />
+        <div className="cardinfo">
+          <span className="cardcate">{props.title}</span>
+          <h3 className="cardtitle">{props.sname}</h3>
+          <a href={props.link} target="blank">
+             <button>Watch now</button>
+          </a>
+       </div>
+    </div>
+
+    </div>
+        </>
+    );
+}
+export default Card;
