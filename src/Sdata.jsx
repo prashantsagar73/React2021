9722ad79f719d53bd0c2ea72328b201ce2f03894
Prashant sagar <prashant.sagar73@gmail.com>
2021-01-22 23:20:00 +0530

    in map method  always give your data a unique id called= key.

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,29 +1,41 @@
 const Sdata= [
     {
+        id: 1,
         sname: "DARK",
         imgsrc:"https://source.unsplash.com/300x200/?netflix,dark",
         title:" A Netflix original series",
         link: "https://www.netflix.com/in/",
     },
     {
+        id: 2,
         sname: "DARK",
         imgsrc:"https://source.unsplash.com/300x200/?Netflix",
         title:" A Netflix original series",
         link:"https://www.netflix.com/in/",
     },
     {
+        id: 3,
         sname: "DARK",
         imgsrc:"https://source.unsplash.com/300x200/?Netflix,Dark",
         title:" A Netflix original series",
         link: "https://www.netflix.com/in/",
     },
     {
+        id: 4,
         sname: "DARK",
         imgsrc:"https://source.unsplash.com/300x200/?dark",
         title:" A Netflix original series",
         link: "https://www.netflix.com/in/",
     },
-    {
+    {    
+        id: 5,
+        sname: "DARK",
+        imgsrc:"https://source.unsplash.com/300x200/?series,dark",
+        title:" A Netflix original series",
+        link: "https://www.netflix.com/in/",
+    },
+    {    
+        id: 6,
         sname: "DARK",
         imgsrc:"https://source.unsplash.com/300x200/?series,dark",
         title:" A Netflix original series",
--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ ReactDOM.render(
    {Sdata.map((val)=> {
       return(
       <Card
+      key ={val.id}
    imgsrc={val.imgsrc}
    title= {val.title}
    sname= {val.sname}
