89a26c57589ea700ae5752f5562275192fe8c5b9
Prashant sagar <prashant.sagar73@gmail.com>
2021-01-22 22:59:54 +0530

    map function in same line

--- src/index.js
+++ src/index.js
@@ -4,22 +4,26 @@ import "./index.css";
 import Card from "./card";
 import Sdata from "./Sdata";
 
-function ncard(val) {
-return(
-<Card
-   imgsrc={val.imgsrc}
-   title= {val.title}
-   sname= {val.sname}
-   link=  {val.link}
-/>);
+// function ncard(val) {
+// return(
+
    
-}
+// }
 
 ReactDOM.render(
     <>
     <h1 className="heading">Top Netflix series</h1>
    
-   {Sdata.map(ncard)}
+   {Sdata.map(function ncard(val) {
+      return(
+      <Card
+   imgsrc={val.imgsrc}
+   title= {val.title}
+   sname= {val.sname}
+   link=  {val.link}
+     />
+     );
+   })}
   
     </>, document.getElementById("root"));
 
