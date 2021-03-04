c6ae19857e9be16cfe8f5e5a0ebf355a64722c8d
Prashant sagar <prashant.sagar73@gmail.com>
2021-03-04 14:25:15 +0530

    Form with two coloum

--- src/Forms.jsx
+++ src/Forms.jsx
@@ -4,23 +4,32 @@ import React, { useState } from "react";
 const Forms=()=>{
     const [name,setname]= useState(" ");
      const [FullName,setfullname]= useState(" ");
- 
+     const [Lastname,setlastname]= useState(" ");
+     const [Getlastname, Getlastnamefinal]= useState();
 
-     const InputEvent=(events)=>{
-        console.log(events.target.value);
-        setname(events.target.value);
+     const InputEvent=(event)=>{
+        console.log(event.target.value);
+        setname(event.target.value);
     };
+    const InputEvent2=(event)=>{
+        setlastname(event.target.value);
+
+    }
     
-    const submit=()=>{
+    const onSubmit =(event)=>{
+        event.preventDefault()
         setfullname(name);
+        Getlastnamefinal(Lastname);
 
     };
     return(
         <>
-        <form onSubmit={onsubmit}>
+        <form onSubmit={onSubmit}>
         <div>
-        <h4>hello {FullName}</h4>
+        <h4>hello {FullName} {Getlastname}</h4>
         <input type="text" placeholder="Enter your name" onChange={InputEvent} value={name}/>
+        <br/>
+        <input type="text" placeholder="Enter your Lastname" onChange={InputEvent2} value={Lastname}/>
         <button type="submit">Click Me🤙</button>
         </div>
         </form>
