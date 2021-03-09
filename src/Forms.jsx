c8faf6f7f87286df698d39c4230b6d52feeb062e
Prashant sagar <prashant.sagar73@gmail.com>
2021-03-09 15:50:41 +0530

    bugs free last one form

--- src/Forms.jsx
+++ src/Forms.jsx
@@ -2,29 +2,45 @@ import React, { useState } from "react";
 
 
 const Forms=()=>{
+    // we can also call function or components,object in hooks useState
     const [fullname,setname]= useState({
-        fname: '', lname: ''
+        fname: "", lname: "",
     });
-    
 
-     const InputEvent=(event)=>{
+     const InputEvent= (event) => {
         console.log(event.target.value);
-        setname(event.target.value);
+        console.log(event.target.name);
+
+            const value = event.target.value;
+            const name = event.target.name;
+
+        setname((preview) => {
+            if (name === 'fname'){
+                return{
+                fname: value,
+                lname: preview.lname,
+            };
+        }else if(name=== 'lname'){
+            return{
+                fname: preview.fname,
+                lname: value,
+
+            };
+        }
+        });
     };
 
-
-    
     // aleart after submiting the form
     const onSubmit =(event)=>{
         event.preventDefault()
-        alert('Form submited');
+        // alert('Form submited');
 
     };
     return(
         <>
         <form onSubmit={onSubmit}>
         <div>
-        <h4>hello {fullname.lname} {fullname.fname} </h4>
+        <h4>hello {fullname.fname} {fullname.lname} </h4>
         <input type="text" placeholder="Enter your name" onChange={InputEvent} name='fname' value={fullname.fname}/>
         <br/>
         <input type="text" placeholder="Enter your Lastname" onChange={InputEvent} name='lname' value={fullname.lname}/>
