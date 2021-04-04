7b007bcfaae5f5e438aeb252f89f639345c3e149
Prashant sagar <prashant.sagar73@gmail.com>
2021-04-04 14:30:30 +0530

    backend of todo app completed

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todo=()=>{
+    const [inputlist,setinputlist]= useState(""); 
+    const [items,setitems]= useState([]);
+
+    const ItemEvent=(event)=>{
+        setinputlist(event.target.value);
+    };
+    const listofitems=()=>{
+        setitems((olditems)=>{
+            return[...olditems,inputlist];
+        });
+        setinputlist('');
+
+    };
     return(
     <>
     <div className="Main_div">
@@ -8,10 +21,19 @@ const Todo=()=>{
     <br/>
     <h1>ToDo List</h1>
     <br/>
-    <input type="text" placeholder="Add a item" />
-    <button> + </button>
+    <input type="text" placeholder="Add a item" value={inputlist} onChange={ItemEvent} />
+    <button onClick={listofitems}> + </button>
     <ol>
-        <li>Buy a apple </li>
+        {items.map((itemval)=>{
+            return( <>
+            <div className="todo_style">
+            <i clas= "fa fa-times" aria-hidden="true"/>
+                <li>{itemval}</li>
+                </div>
+                </>
+            );
+        })
+        }
     </ol>
     </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -11,3 +11,4 @@ ReactDOM.render(
 // const fname=(a,b)=> a+b;
 // this is fat Arrow (=>) Function 
 // insted of function we fat arrow function 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
